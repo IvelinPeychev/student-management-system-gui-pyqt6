dbce5216b7e45317a9400a48ad0ae47015dd37fa
Ivelin Peychev <peychev.vn@gmail.com>
2023-05-14 20:01:44 +0300

    Day 45: - done with homework

--- homework.py
+++ homework.py
@@ -18,31 +18,40 @@ class AgeCalculator(QWidget):
         time_label = QLabel('Time (hours): ')
         self.time_line_edit = QLineEdit()
 
-        calculate_button = QPushButton(' Calculate speed')
-        calculate_button.clicked.connect(self.calculate_age)
-        self.output_label = QLabel('')
 
         self.system = QComboBox()
         self.system.addItems(['Metrical (km)', 'Imperial (miles)'])
+
+        calculate_button = QPushButton(' Calculate speed')
+        calculate_button.clicked.connect(self.calculate_speed)
+
+        self.output_label = QLabel('')
 
         # Add widgets to grid
 
         grid.addWidget(distance_label, 0, 0)
         grid.addWidget(self.distance_line_edit, 0, 1)
+        grid.addWidget(self.system, 0, 2)
         grid.addWidget(time_label, 1, 0)
         grid.addWidget(self.time_line_edit, 1, 1)
         grid.addWidget(calculate_button, 3, 1)
-        grid.addWidget(self.output_label, 4, 0, 1, 1)
-        grid.addWidget(self.system, 0, 2)
+        grid.addWidget(self.output_label, 3, 0, 1, 1)
 
         self.setLayout(grid)
 
-    def calculate_age(self):
-        current_year = datetime.now().year
-        date_of_birth = self.time_line_edit.text()
-        year_of_birth = datetime.strptime(date_of_birth, '%m/%d/%Y').date().year
-        age = current_year - year_of_birth
-        self.output_label.setText(f'{self.distance_line_edit.text()} is {age} years old')
+    def calculate_speed(self):
+        distance = float(self.distance_line_edit.text())
+        time = float(self.time_line_edit.text())
+        speed = distance / time
+        match self.system.currentText():
+            case 'Metrical (km)':
+                speed = round(speed, 2)
+                point = 'km/h'
+            case 'Imperial (miles)':
+                speed = round(speed*0.621371, 2)
+                point = 'm/h'
+
+        self.output_label.setText(f'Average speed: {speed} {point}')
 
 
 
